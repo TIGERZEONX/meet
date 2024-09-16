151ac33be6a79a1bff3d49ac0085396c5a904b42
TIGERZEONX <gautham3177@gmail.com>
2024-09-16 22:47:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+// Function to display current time in UTC
 function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+    document.getElementById('currentTime').innerHTML = new Date().toUTCString();
 }
 showTime();
 setInterval(function () {
-	showTime();
+    showTime();
 }, 1000);
+
+// Other script functionalities (assuming your existing code for peer connection or buttons are here)
